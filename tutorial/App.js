89e7ffb697d019a13e8585ce0e7d33bcdd01b5e1
Wilson <wilson.l20010327@gmail.com>
2022-08-02 23:21:02 +0800

    tutorial: connect text and the textinput with usestate variable

--- tutorial/App.js
+++ tutorial/App.js
@@ -1,4 +1,5 @@
 import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
 import {
   StyleSheet,
   Text,
@@ -9,20 +10,22 @@ import {
 } from "react-native"; //before using the components make sure to import them
 
 export default function App() {
+  const [text, SetText] = useState("I am text"); // define a variable which is "I am text" as default and can chage the value though using the function SetText
   return (
     <View style={styles.container}>
       <View style={styles.black}>
         <View style={styles.container_black}>
           <View style={styles.pink}>
             <Text style={styles.text_font /*give the text propety */}>
-              I am Text
+              {text}
             </Text>
             {/* this component can show the words whcih are between*/}
           </View>
           <View style={styles.black}>
             <TextInput
               style={styles.input}
-              placeholder="type word" //the default text will show
+              placeholder="type word to change text" //the default text will show
+              onChangeText={SetText} // the action when the input textinput has change
             />
           </View>
         </View>
@@ -48,7 +51,7 @@ const styles = StyleSheet.create({
   },
   pink: {
     flex: 1,
-    justifyContent: "center",// move the component to center
+    justifyContent: "center", // move the component to center
     backgroundColor: "pink", // change the color of the View to make the result of flex more visible
   },
   container_black: {
@@ -58,9 +61,9 @@ const styles = StyleSheet.create({
   },
   input: {
     height: 40,
-    margin:50, // change the witdh of the margin 
-    borderColor:'red', //change the border's color
-    borderWidth: 1,// change the witdh of the border
+    margin: 50, // change the witdh of the margin
+    borderColor: "red", //change the border's color
+    borderWidth: 1, // change the witdh of the border
     padding: 10, //add so space around the component
   },
 });
