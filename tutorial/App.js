4727895b7091cebcbf6d666269f4de90ab8aca65
wilson20010327 <wilson.l20010327@gmail.com>
2022-08-16 14:36:06 +0800

    tutorial: useful component finish, type in the textinput and press the customize button then the alert will pop out, beside pressing other part of the screen the keyboard will dismiss

--- tutorial/App.js
+++ tutorial/App.js
@@ -3,102 +3,54 @@ import { useState } from "react";
 import {
   StyleSheet,
   View,
-  FlatList,
   Text,
   TouchableOpacity,
-  Button,
+  Alert,
+  TextInput,
+  Keyboard,
   Dimensions,
+  TouchableWithoutFeedback,
 } from "react-native"; //before using the components make sure to import them
-const DATA = [
-  {
-    id: 1,
-    title: "First Item",
-  },
-  {
-    id: 2,
-    title: "Second Item",
-  },
-  {
-    id: 3,
-    title: "Third Item",
-  },
-  {
-    id: 4,
-    title: "Four Item",
-  },
-  {
-    id: 5,
-    title: "Five Item",
-  },
-  {
-    id: 6,
-    title: "Six Item",
-  },
-  {
-    id: 7,
-    title: "Seven Item",
-  },
-  {
-    id: 8,
-    title: "Eight Item",
-  },
-  {
-    id: 9,
-    title: "Nine Item",
-  },
-];
-//get the window size
+
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
 
 export default function App() {
-  var [data, setData] = useState(DATA); // make the data flexible
-  var [isFetching, setIsFetching] = useState(false);
-  var itemOnPress = (id) => {
-    setData((prev) => {
-      return prev.filter((data) => data.id != id);
-    });
+  var [input, setInput] = useState("");
+  var buttonOnPress = () => {
+    {/*whwn the button been pressed the alert dialog will pop out, you can change the title, msg and the button bellow the dialog */ }
+    Alert.alert("This is alert", input,
+      [
+        {
+          text: "Cancel",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel"
+        },
+        { text: "OK", onPress: () => console.log("OK Pressed") }
+      ]);
   };
-  /* make the component has the action when the press event happened*/
-  const renderItem = ({ item }) => (
-    <TouchableOpacity onPress={() => itemOnPress(item.id)}>
-      <View style={styles.item}>
-        <Text>{item.title}</Text>
-      </View>
-    </TouchableOpacity>
-  );
   return (
-    <View style={styles.container}>
-      <View style={styles.black}>
-        <View style={styles.container_black}>
-          <View style={styles.white}>
-            {/*list the data which is .json like data*/}
-            <FlatList
-              data={data} //specify the data
-              renderItem={
-                renderItem
-              } /*each item in the data will be the argument of this function*/
-              onRefresh={() => {
-                //it is not necessary, can refreah when slide to the top
-                setIsFetching(true); 
-                setData(DATA);
-                setIsFetching(false);
-              }}
-              refreshing={isFetching}// it is necessary when you want to use onrefresh 
-              //keyExtractor={(item) => item.id} you can change the order of the list
-            />
-            
-            <Button
-              style={{ paddingBottom: 20 }}
-              title="refresh"
-              onPress={() => {
-                setData(DATA);
-              }}
-            />
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss() /*pack all component in this project when the touch screen event happened but without any feetback then implement keyboard dismiss */}> 
+      <View style={styles.container}>
+        <View style={styles.black}>
+          <View style={styles.container_black}>
+            <View style={styles.pink}>
+              <View style={{ flexDirection: "row" }}>
+                <TextInput style={styles.input}
+                  placeholder="Type word" //the default text will show
+                  onChangeText={setInput}
+                />
+                <TouchableOpacity onPress={buttonOnPress}>
+                  <View style={styles.button}>
+                    <Text >Ready</Text>
+                  </View>
+                </TouchableOpacity>
+              </View>
+            </View>
           </View>
         </View>
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -119,9 +71,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   white: {
-    paddingTop: 50,
     flex: 1,
-    paddingBottom: 20,
     //justifyContent: "center", // move the component to center
     backgroundColor: "white", //change the color of the View to make the result of flex more visible
     //alignItems: "center", //  align the child in ths component to center
@@ -137,13 +87,17 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     justifyContent: "center",
   },
-  item: {
-    backgroundColor: "orange",
+  input: {
     width: windowWidth * 0.8,
     height: windowHeight * 0.1,
-    marginVertical: 8,
-    marginHorizontal: 16,
+    borderColor: "red", //change the border's color
+    borderWidth: 1, // change the witdh of the border
+  },
+  button: {
+    width: windowWidth * 0.2,
+    height: windowHeight * 0.1,
     justifyContent: "center",
-    alignItems: "center",
+    alignItems: 'center',
+    backgroundColor: 'orange'
   },
 });
